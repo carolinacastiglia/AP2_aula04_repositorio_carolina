432e72db1e0331c0309ae78bd8c77671f71ecc81
carolinacastiglia <carol_castiglia@hotmail.com>
2021-04-01 10:31:11 -0300

    Create Moto.py
    criação classe Moto com seu construtor e método imprimir informações.

---
+++ Moto.py
@@ -0,0 +1,13 @@
+from Automovel import Automovel
+
+class Moto(Automovel):
+    def __init__(self, marca, qtdRodas, modelo, velocidade, potenciaDoMotor, partidaEletrica):
+        Automovel.__init__(self, marca, qtdRodas, modelo, velocidade, potenciaDoMotor)
+        self.partidaEletrica = partidaEletrica
+    
+    def imprimirInformacoes(self):
+        Automovel.imprimirInformacoes(self)
+        if self.partidaEletrica == 1:
+            print("Com partida elétrica")
+        else:
+            print("Sem partida elétrica")
