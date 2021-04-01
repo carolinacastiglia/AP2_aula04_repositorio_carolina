fdf93ff8cfd00bede8bf9de866f99f1b5f515683
carolinacastiglia <carol_castiglia@hotmail.com>
2021-04-01 10:43:38 -0300

    Create Carro.py
    criação classe Carro com seu construtor e método imprimir informações.

---
+++ Carro.py
@@ -0,0 +1,10 @@
+from Automovel import Automovel
+
+class Carro(Automovel):
+    def __init__(self, marca, qtdRodas, modelo, velocidade, potenciaDoMotor, qtdPortas):
+        Automovel.__init__(self, marca, qtdRodas, modelo, velocidade, potenciaDoMotor)
+        self.qtdPortas = qtdPortas
+    
+    def imprimirInformacoes(self):
+        Automovel.imprimirInformacoes(self)
+        print("Possui", self.qtdPortas, "portas")
